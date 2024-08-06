ce2b841b3c8377fd215de7f058c80a2342735e6a
Falts42 <zach@falter.org>
2024-08-06 15:21:27 -0600

    Added ability to update items

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("My API is up and running Yo!");
 });
 
-//===========================GET All Items in Inventory==========================================
+//===========================GET ALL Items in Inventory==========================================
 
 app.get('/inventory', (req, res) => {
   knex('items').select('*')
@@ -47,6 +47,43 @@ app.get("/inventory/:item_name", async (req, res) => {
   } catch (error) {
     console.error("error fetching inventory", error);
     return res.status(500).json({ error: "failed to fetch inventory" + error });
+  }
+});
+
+//=======================POST NEW Item=============================================
+app.post("/inventory", async (req, res) => {
+  const { itemData } = req.body;
+
+  if (!itemData) {
+    return res.status(400).json({ error: "empty item field" });
+  }
+  try {
+    const user = await knex("items").insert({ item_name: itemData }); //.returning("id");
+    return res.status(202).json({ message: "item successfully added" });
+  } catch (error) {
+    console.error("error adding item", error);
+    return res.status(500).json({ error: "failed to add item" });
+  }
+});
+
+//=======================UPDATE Item=============================================
+app.patch("/inventory/:item_name", async (req, res) => {
+  const { item_name } = req.params;
+  const { description, quantity} = req.body;
+
+  try {
+    const update = await knex("items")
+      .where({ item_name: item_name })
+      .update({ description: description, quantity: quantity });
+
+    if (update === 0) {
+      return res.status(404).json({ error: "item not found" });
+    } else {
+      return res.status(202).json("Item updated successfully");
+    }
+  } catch (error) {
+    console.error("error updating item ", error);
+    return res.status(500).json({ error: "failed to update item" });
   }
 });
 
