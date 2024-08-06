223020d9771c051fecbfe3e3fb676ba9d013a8a4
Falts42 <zach@falter.org>
2024-08-06 14:01:49 -0600

    Added some GET routes

--- api/index.js
+++ api/index.js
@@ -17,4 +17,38 @@ app.get("/", (req, res) => {
   res.send("My API is up and running Yo!");
 });
 
+//===========================GET All Items in Inventory==========================================
+
+app.get('/inventory', (req, res) => {
+  knex('items').select('*')
+    .then(data => res.status(200).json(data))
+});
+
+//===========================GET Item by Name==========================================
+
+
+app.get("/inventory/:item_name", async (req, res) => {
+  const { item_name } = req.params;
+  if (!item_name) {
+    return res
+      .status(400)
+      .json({ error: "empty item name field" });
+  }
+
+  try {
+    const item = await knex("items")
+      .select("*")
+      .where({ item_name: item_name })
+      .first();
+
+    if (!item) return res.status(404).json({ error: "item not found" });
+
+    return res.json(item);
+  } catch (error) {
+    console.error("error fetching inventory", error);
+    return res.status(500).json({ error: "failed to fetch inventory" + error });
+  }
+});
+
+
 app.listen(port, () => console.log('server is up and running'));
